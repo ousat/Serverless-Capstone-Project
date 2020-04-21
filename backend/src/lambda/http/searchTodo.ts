f79138c1136f6d4c703771f61372fc83968b1d7f
Satish Kumar <s.n.kumar.satish.sk@gmail.com>
2020-04-21 12:50:06 +0530

    search todo function added

---
+++ backend/src/lambda/http/searchTodo.ts
@@ -0,0 +1,49 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { searchNewTodoInES, searchDoneTodoInES } from '../../domain/elasticSearch'
+import { SearchOnTodoType } from '../../models/SearchOnTodoType'
+import { extractToken } from '../../auth/utils'
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  
+  const request = JSON.parse(event.body)
+  
+  
+  const jwtToken = extractToken(event.headers.Authorization)
+
+  console.log("Received request",request)
+  console.log("Query is :",request.query)
+  let results:any
+
+  if(request.on === SearchOnTodoType.PENDING){
+    results = await searchNewTodoInES(request.query,jwtToken)
+  }else if(request.on =SearchOnTodoType.DONE){
+    results = await searchDoneTodoInES(request.query,jwtToken)
+  }else{
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        results:"Invalid param value for On parameter"
+      })
+    }
+  }
+
+  const resturnRes = JSON.parse(JSON.stringify(results)).hits
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      resturnRes
+    })
+  }
+})
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
